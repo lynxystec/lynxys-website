231b97001aaead884e69626201ad64144e7649c9
Lynxys Tecnologia <lynxystecnologia@gmail.com>
2025-03-23 12:01:09 -0300

    fix: host base url

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,21 @@
 function goTo(route) {
   const HOST = location.origin;
 
-  if (!route) {
-    location.href = `${HOST}/`;
-    return;
+  if (test.startsWith('http://')) {
+    if (!route) {
+      location.href = `${HOST}/`;
+      return;
+    }
+  
+    location.href = `${HOST}/${route}`;
   }
 
-  location.href = `${HOST}/${route}`;
+  if (test.startsWith('https://')) {
+    if (!route) {
+      location.href = `${HOST}/lynxys-website`;
+      return;
+    }
+  
+    location.href = `${HOST}/lynxys-website/${route}`;
+  }
 }
