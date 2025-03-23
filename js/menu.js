5bc9954ded2e2c2a8a36f7d3013bfa5b3da9b6f2
Lynxys Tecnologia <lynxystecnologia@gmail.com>
2025-03-23 11:31:22 -0300

    chore: change prod host url

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
 function goTo(route) {
+  const GITHUB_PAGES_HOST = 'lynxys-website';
+
   if (!route) {
-    location.href = '/';
+    location.href = `${GITHUB_PAGES_HOST}/`;
     return;
   }
 
-  location.href = `/${route}`;
+  location.href = `${GITHUB_PAGES_HOST}/${route}`;
 }
